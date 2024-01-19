56097a96e7913ba747b83df3973b745dddb254df
InkyungLee <yklee980216@naver.com>
2024-01-19 17:40:15 +0900

    no message

---
+++ js/00_input에저장된값모두더하기.js
@@ -0,0 +1,15 @@
+/* <main class="container">
+<div class="btnBox">
+<button id="createBtn">추가</button>
+<button id="calculateBtn">계산</button>
+</div>
+<div class="inputBox"></div> 
+</main>*/
+const createBtn = document.querySelector("#createBtn");
+const container = document.querySelector(".container");
+createBtn.addEventListener("click", ()=>{
+    const input = document.createElement("input");
+    input.classList.add("input");
+    container.append(input);
+    const
+});
